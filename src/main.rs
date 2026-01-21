1701198d2cdaed7c506320a94d7b40bd51b8b2ef
radiating-pumpkin0Zed <a2zthisisforearningmoney@gmail.com>
2026-01-21 16:30:27 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,8 @@ use std::io::{self, Write};
 
 fn main() {
 
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    // TODO: Read user input and execute commands
     let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    println!("{}: command not found", input.trim());
-
+  
     // TODO: Implement the REPL(Read-Eval-Print Loop)
     loop {
         print!("$ ");
@@ -23,8 +17,15 @@ fn main() {
         if command == "exit" {
             break;
         }
+        
+        //Echo command
+        let parts: Vec<&str> = command.split_whitespace().collect();
 
-        // Here you would normally parse and execute the command
-        println!("{}: command not found", command);
+        if !parts.is_empty() && parts[0] == "echo" {
+            println!("{}", parts[1..].join(" "));
+            continue;
+        }
+
+    println!("{}: command not found", command);
     }
 }
