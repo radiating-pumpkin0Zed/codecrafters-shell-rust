7d9fa91042302dfbcfe1f36883161fe34253d27c
radiating-pumpkin0Zed <a2zthisisforearningmoney@gmail.com>
2026-01-21 16:02:32 +0000

    can read user input and execute the commands

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
+
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    // TODO: Read user input and execute commands
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    println!("{}: command not found", input.trim());
 }
