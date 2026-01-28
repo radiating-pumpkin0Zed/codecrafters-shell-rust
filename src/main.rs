8749467d093fd03e5a9084869677a738b44a2d6e
radiating-pumpkin0Zed <a2zthisisforearningmoney@gmail.com>
2026-01-28 15:33:07 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,59 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
+const BUILT_IN_COMMANDS: [&str; 3] = ["echo", "exit", "type"];
+enum Command {
+    ExitCommand,
+    EchoCommand { display_string: String },
+    TypeCommand { command_name: String },
+    CommandNotFound,
+}
 
-    let mut input = String::new();
-  
-    // TODO: Implement the REPL(Read-Eval-Print Loop)
+impl Command {
+    fn from_input(input: &str) -> Self {
+        let input = input.trim();
+        if input == "exit" {
+            return Self::ExitCommand;
+        };
+        if let Some(pos) = input.find("echo ") {
+            if pos == 0 {
+                return Self::EchoCommand {
+                    display_string: input["echo ".len()..].to_string(),
+                };
+            }
+        }
+        if let Some(pos) = input.find("type ") {
+            if pos == 0 {
+                return Self::TypeCommand {
+                    command_name: input["type ".len()..].to_string(),
+                };
+            }
+        }
+        Self::CommandNotFound
+    }
+}
+
+fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
-
-        input.clear();
-        io::stdin().read_line(&mut input).unwrap();
-        let command = input.trim();
-
-        if command == "exit" {
-            break;
-        }
         
-        //Echo command
-        let parts: Vec<&str> = command.split_whitespace().collect();
-
-        if !parts.is_empty() && parts[0] == "echo" {
-            println!("{}", parts[1..].join(" "));
-            continue;
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        let command = Command::from_input(&input);
+        
+        match command {
+            Command::ExitCommand => break,
+            Command::EchoCommand { display_string } => println!("{}", display_string),
+            Command::TypeCommand { command_name } => {
+                if BUILT_IN_COMMANDS.contains(&command_name.as_str()) {
+                    println!("{} is a shell builtin", command_name);
+                } else {
+                    println!("{}: not found", command_name)
+                }
+            }
+            Command::CommandNotFound => println!("{}: command not found", input.trim()),
         }
-
-    println!("{}: command not found", command);
     }
 }
